16db5983e172567d40c74b97b6534aa51b48d2d8
“theodoreheckelman” <tjheck242@gmail.com>
2025-03-01 18:30:01 -0700

    fix(Dependencies): include nflplot dependency

--- init.R
+++ init.R
@@ -1,3 +1,3 @@
-install.packages(c("shiny", "shinylive", "tidyverse"), repos='https://cloud.r-project.org/')
+install.packages(c("shiny", "shinylive", "tidyverse", "remotes"), repos='https://cloud.r-project.org/')
 install.packages("shinythemes")
-
+remotes::install_github("thomas-mullen/nflplotR")
