44ed69877f3f31e939876245ffbc45fcc8c4aa1d
“theodoreheckelman” <tjheck242@gmail.com>
2025-03-01 20:35:27 -0700

    fix(Dependencies): add more missing dependencies

--- init.R
+++ init.R
@@ -1,2 +1,7 @@
-install.packages(c("shiny", "shinylive", "tidyverse", "ggpath", "magick", "nflplotR"), repos='https://cloud.r-project.org/')
+install.packages(c("shiny", "shinylive", "tidyverse"), repos='https://cloud.r-project.org/')
 install.packages("shinythemes")
+install.packages("magick", repos = "https://cloud.r-project.org/")
+install.packages("archive", repos = "https://cloud.r-project.org/")
+install.packages("ggpath", repos = "https://cloud.r-project.org/")
+install.packages("nflplotR", repos = "https://cloud.r-project.org/")
+
